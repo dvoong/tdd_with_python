3260efd27262e5407ea6058397375dcf3e822164
David Voong <voong.david@gmail.com>
2015-06-30 12:46:22 +0100

    Added new list view that displays the list

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,21 @@ from lists.models import Item
 
 from lists.views import home_page
 
+class ListViewTest(TestCase):
+    
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 class HomePageTest(TestCase):
     
     def test_root_url_resolves_to_home_page_view(self):
@@ -42,18 +57,8 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
 class ItemModelTest(TestCase):
     
     def test_saving_and_retrieving_items(self):
