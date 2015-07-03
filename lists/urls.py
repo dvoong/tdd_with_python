e9d7fb55eeae271f6b250e208144865489b905cd
David Voong <voong.david@gmail.com>
2015-07-03 14:35:41 +0100

    Refactor list view to handle item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -19,5 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item, List
 
-# Create your views here.
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-
 def new_list(request):
     list_ = List.objects.create()
     new_item_text = request.POST['item_text']
@@ -25,8 +19,11 @@ def new_list(request):
     return redirect('/lists/%d/' % (list_.id,))
 
 def view_list(request, list_id):
-    list = List.objects.get(id=list_id)
-    return render(request, 'lists/list.html', {'list': list})
+    list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id))
+    return render(request, 'lists/list.html', {'list': list_})
 
 def home_page(request):
     return render(request, 'lists/home.html')
