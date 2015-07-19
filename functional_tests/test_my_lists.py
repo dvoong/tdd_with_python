7b67e0f89c7ccbbd9e969b043241931adc5f7bdb
David Voong <voong.david@gmail.com>
2015-07-19 10:20:10 +0100

    Trying to fix the live server session store

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -30,6 +30,19 @@ class MyListsTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
 
         self.browser.get(self.server_url)
+        from django.conf import settings
+        from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+        User = get_user_model()
+        from django.contrib.sessions.backends.db import SessionStore
+#         print('create_session.py: backend_session: {}'.format(session))
+#         print('SESSION_KEY:', SESSION_KEY)
+#         print('session[SESSION_KEY]:', session[SESSION_KEY])
+#         print('BACKEND_SESSION_KEY:', BACKEND_SESSION_KEY)
+#         print('session[BACKEND_SESSION_KEY]:', session[BACKEND_SESSION_KEY])
+#         print('session.keys():', session.keys())
+#         print('session.session_key:', session.session_key)
+#         print('type(session.session_key)', type(session.session_key))
+
         print('test_my_lists.py: cookies: {}'.format(self.browser.get_cookies()))
         self.wait_to_be_logged_in(email)
 
