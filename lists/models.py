b63b6243667a93c632323d3c9012101e0946ee30
David Voong <voong.david@gmail.com>
2015-07-19 12:12:41 +0100

    Reset the code back to failing because it doesn't know how to save a user to a list. Setting up a more test isolation method

--- lists/models.py
+++ lists/models.py
@@ -4,13 +4,8 @@ from django.conf import settings
 
 
 class List(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
-
-    @property
-    def name(self):
-        return self.item_set.first().text
 
 class Item(models.Model):
     text = models.TextField(default='')
